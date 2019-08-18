96c3d7d3221373011bea6e9f289a3133bb943c87
Joapfel <jojokr94@gmail.com>
2019-08-18 17:45:43 +0200

    implemented reaction on speech button press

--- chatbot/chat_gui.py
+++ chatbot/chat_gui.py
@@ -1,12 +1,12 @@
 # This Python file uses the following encoding: utf-8
-import sys
-from functools import partial
-from PyQt5 import QtWidgets, uic, QtCore
-from houndify_api.houndify import TextHoundClient
-from houndify_api.settings import CLIENT_KEY, CLIENT_ID, USER, REQUEST_INFO
+from PyQt5 import QtWidgets, uic, QtCore, QtGui
+from houndify_api.houndify import TextHoundClient, StreamingHoundClient
+from houndify_api.settings import CLIENT_KEY, CLIENT_ID, USER, REQUEST_INFO, FORMAT, CHANNELS, RATE, CHUNK, BUFFER_SIZE
+from houndify_api.speech_listener import SpeechListener
 from mercedes_api.mercedes_cv_api import MercedesCvApi as MercedesApi
 from mercedes_api.settings import AUTH_TOKEN, VEHICLE_ID
 from mercedes_api.mercedes_response_handler import MercedesResponseHandler
+import pyaudio
 from chatbot.settings import ROLE_DESIGN_MAPPING
 import traceback
 from loguru import logger
@@ -16,23 +16,64 @@ class ChatGUI(QtWidgets.QMainWindow):
 
     def __init__(self):
         super(ChatGUI, self).__init__()
+        # load the static gui
+        # and define the layout size
         self.ui = uic.loadUi('qt5chatgui.ui', self)
         self.scroll_area = self.findChild(QtWidgets.QScrollArea, 'scroll_area')
-        # self.vertical_bar = self.scroll_area.verticalScrollBar()
         self.scroll_area_widget_contents = self.findChild(QtWidgets.QWidget, 'scroll_area_widget_contents')
         self.layout = self.scroll_area_widget_contents.layout()
-        # self.layout.setColumnStretch(0, 2)
-        # self.layout.setColumnStretch(2, 4)
+        self.layout.setColumnStretch(0, 4)
+        self.layout.setColumnStretch(4, 8)
 
+        # text input and speech button connected with their event handlers
         self.text_input = self.findChild(QtWidgets.QLineEdit, 'text_input')
         self.text_input.returnPressed.connect(self.on_enter)
+        self.speech_button = self.findChild(QtWidgets.QPushButton, 'speech_button')
+        self.speech_button.pressed.connect(self.on_speech)
 
+        # create initial chat bubble with a greeting
         # gets overridden after every request
-        self.last_chat_bubble = self.findChild(QtWidgets.QLabel, 'label_greet')
+        greeting = 'How can I help you?'
+        self.last_chat_bubble = QtWidgets.QLabel(greeting)
+        self.last_chat_bubble.setFont(QtGui.QFont('Ubuntu', 16))
+        self.last_chat_bubble.setAlignment(QtCore.Qt.AlignCenter)
+        self.last_chat_bubble.setStyleSheet(ROLE_DESIGN_MAPPING['bot'])
+        self.last_chat_bubble.setFixedSize(self._label_size(greeting), 51)
+        self.layout.addWidget(self.last_chat_bubble, 0, 0)
         self.show()
 
+        # connections to houndify and mercedes apis
         self.houndify_client = TextHoundClient(CLIENT_ID, CLIENT_KEY, USER, REQUEST_INFO)
         self.mercedes = MercedesApi(AUTH_TOKEN)
+
+    def handle_houndify_response(self, res):
+        try:
+            user_result = res['AllResults'][0]['WrittenResponseLong']
+            self.last_chat_bubble = self.add_chat_bubble(text=user_result,
+                                                         last_bubble=self.last_chat_bubble,
+                                                         role='bot')
+        except KeyError:
+            logger.warning(str(res))
+            try:
+                if res['Error'] == b'Over daily limit\n':
+                    self.last_chat_bubble = self.add_chat_bubble(text='Over daily limit at Houndify :(',
+                                                                 last_bubble=self.last_chat_bubble,
+                                                                 role='bot')
+            except:
+                pass
+
+        try:
+            intent = res['AllResults'][0]['Result']
+            if 'car' in intent.keys():
+                res, vehicle_part, user_result = MercedesResponseHandler.on_mercedes_indent(intent['car'],
+                                                                                            self.mercedes, VEHICLE_ID)
+                if user_result:
+                    self.add_chat_bubble_big_text(text=user_result)
+                elif res:
+                    user_result = MercedesResponseHandler.json_information_response_readable(res, vehicle_part)
+                    self.add_chat_bubble_big_text(text=user_result)
+        except KeyError:
+            logger.warning(traceback.format_exc())
 
     @QtCore.pyqtSlot()
     def on_enter(self):
@@ -43,46 +84,56 @@ class ChatGUI(QtWidgets.QMainWindow):
                                                      role='user')
         # request to Houndify
         res = self.houndify_client.query(request)
-        user_result = res['AllResults'][0]['WrittenResponseLong']
-        self.last_chat_bubble = self.add_chat_bubble(text=user_result,
-                                                     last_bubble=self.last_chat_bubble,
-                                                     role='bot')
+        self.handle_houndify_response(res)
 
+    @QtCore.pyqtSlot()
+    def on_speech(self):
+        speech_client = StreamingHoundClient(CLIENT_ID, CLIENT_KEY, USER, REQUEST_INFO)
+        speech_client.start(SpeechListener(on_partial_transcript_func=self))
+
+        p = pyaudio.PyAudio()
+        stream = p.open(format=FORMAT, channels=CHANNELS,
+                        rate=RATE, input=True, frames_per_buffer=CHUNK)
+
+        while True:
+            samples = stream.read(BUFFER_SIZE)
+            if len(samples) == 0:
+                break
+            if speech_client.fill(samples):
+                break
+
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
+        res = speech_client.finish()
+        user_utterance = 'Sorry, your request was not recognized.'
         try:
-            intent = res['AllResults'][0]['Result']
-            if 'car' in intent.keys():
-                res, vehicle_part, user_result = MercedesResponseHandler.on_mercedes_indent(intent['car'], self.mercedes, VEHICLE_ID)
-                if user_result:
-                    self.add_chat_bubble_big_text(text=user_result)
-                elif res:
-                    user_result = MercedesResponseHandler.json_information_response_readable(res, vehicle_part)
-                    self.add_chat_bubble_big_text(text=user_result)
-        except KeyError:
-            logger.warning(traceback.format_exc())
+            user_utterance = res['Disambiguation']['ChoiceData'][0]['Transcription']
+        except:
+            pass
+        self.last_chat_bubble = self.add_chat_bubble(user_utterance, self.last_chat_bubble, role='user')
+        self.handle_houndify_response(res)
 
     def add_chat_bubble(self, text, last_bubble, role='bot'):
         last_rect = last_bubble.geometry()
-        x = last_rect.x()
-        y = last_rect.y()
-        width = last_rect.width()
         height = last_rect.height()
 
         label = QtWidgets.QLabel(text)
         label.setFont(last_bubble.font())
+
         label.setAlignment(QtCore.Qt.AlignCenter)
         label.setStyleSheet(ROLE_DESIGN_MAPPING[role])
         label.setFixedSize(self._label_size(text), height)
 
         new_x = 0
         if role == 'user':
-            new_x = 1
+            new_x = 8
         new_y = self.layout.indexOf(last_bubble) + 1
 
         self.layout.addWidget(label, new_y, new_x)
         label.show()
 
         self.scroll_area.ensureWidgetVisible(label)
-        #QtCore.QTimer.singleShot(0, partial(self.scroll_area.ensureWidgetVisible, label))
         return label
 
     def add_chat_bubble_big_text(self, text):
